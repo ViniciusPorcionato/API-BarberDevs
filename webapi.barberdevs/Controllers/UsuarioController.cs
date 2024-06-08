2044b9f4d481020c16ef6012095792b223e53fa7
Vinicius <porcionatovinicius@gmail.com>
2024-06-08 10:03:10 -0300

    Adicionado UsuarioController

---
+++ webapi.barberdevs/Controllers/UsuarioController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using webapi.barberdevs.Interfaces;
+using webapi.barberdevs.Repositories;
+using webapi.barberdevs.ViewModel;
+
+namespace webapi.barberdevs.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        private IUsuarioRepository _usuarioRepository;
+
+        public UsuarioController()
+        {
+            _usuarioRepository = new UsuarioRepository();
+        }
+
+        [HttpPut("AlterarSenha")]
+        public IActionResult UpdatePassword(string email, AlterarSenhaViewModel senha)
+        {
+            try
+            {
+                _usuarioRepository.AlterarSenha(email, senha.SenhaNova!);
+                
+                return Ok("Senha alterada com sucesso !");
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+                throw;
+            }
+        }
+
+        [HttpGet("BuscarPorId")]
+        public IActionResult GetById(Guid id)
+        {
+            try
+            {
+                return Ok(_usuarioRepository.BuscarPorId(id));
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+                throw;
+            }
+        }
+    }
+}
