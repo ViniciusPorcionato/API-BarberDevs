dc7e33621e4ac57b3a0b81a5e6049144a4943ffd
Miguel Lamarca Moreira <50866670890@info.local>
2024-06-17 17:14:47 -0300

    att

--- webapi.barberdevs/Contexts/BarberDevsContext.cs
+++ webapi.barberdevs/Contexts/BarberDevsContext.cs
@@ -30,7 +30,7 @@ public partial class BarberDevsContext : DbContext
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
     #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-            => optionsBuilder.UseSqlServer("Data Source=NOTE13-S21; initial catalog=BarberDevs; User Id = sa; pwd = Senai@134; TrustServerCertificate=true;");
+            => optionsBuilder.UseSqlServer("Data Source=NOTE16-S21; initial catalog=BarberDevs; User Id = sa; pwd = Senai@134; TrustServerCertificate=true;");
 
 //    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
