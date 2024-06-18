8f9fbbf8cf27173d5a712efbf8cced2be25784a4
Vinicius <porcionatovinicius@gmail.com>
2024-06-18 14:07:10 -0300

    .

--- webapi.barberdevs/obj/Debug/net7.0/webapi.barberdevs.AssemblyInfo.cs
+++ webapi.barberdevs/obj/Debug/net7.0/webapi.barberdevs.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webapi.barberdevs")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+582e065030977ae4bd1abf404e40ec44424b74b0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fa26e0063a961d6933cf6463840ea0f730dc078")]
 [assembly: System.Reflection.AssemblyProductAttribute("webapi.barberdevs")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webapi.barberdevs")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
